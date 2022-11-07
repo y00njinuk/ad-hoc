ce7c299061889f142121a7c31f11d90c056730ae
y00njinuk <jinuknamja@gmail.com>
2022-11-07 23:48:06 +0900

    init

---
+++ src/main/StreamBenchmarkAndTutorial/StreamEx.java
@@ -0,0 +1,13 @@
+package StreamBenchmarkAndTutorial;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamEx {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5).stream();
+
+        stream.map(e -> { System.out.println("Calling by Intermediate operation: " + e); return e;})
+              .forEach(e -> System.out.println("Calling by Terminal operation: " + e));
+    }
+}
