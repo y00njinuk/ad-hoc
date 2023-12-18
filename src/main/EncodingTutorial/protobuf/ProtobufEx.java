136358e16f52b2551c785467f03d57f1ecbfea66
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-12-18 23:22:35 +0900

    add: convert from protobuf to json by using "protobuf-java-util"

--- src/main/EncodingTutorial/protobuf/ProtobufEx.java
+++ src/main/EncodingTutorial/protobuf/ProtobufEx.java
@@ -1,10 +1,26 @@
 package EncodingTutorial.protobuf;
 
+import com.google.protobuf.Message;
+import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.Struct;
+import com.google.protobuf.util.JsonFormat;
 import org.apache.commons.codec.binary.Hex;
 import EncodingTutorial.protobuf.schema.Person;
 
+import java.io.IOException;
+
 public class ProtobufEx {
-    public static void main(String[] args) {
+    public static Message fromJson(String json) throws IOException {
+        Message.Builder structBuilder = Struct.newBuilder();
+        JsonFormat.parser().ignoringUnknownFields().merge(json, structBuilder);
+        return structBuilder.build();
+    }
+
+    public static String toJson(MessageOrBuilder messageOrBuilder) throws IOException {
+        return JsonFormat.printer().print(messageOrBuilder);
+    }
+
+    public static void main(String[] args) throws IOException {
         Person person = Person.newBuilder()
                 .setUserName("Martin")
                 .setFavoriteNumber(1337)
@@ -15,6 +31,10 @@ public class ProtobufEx {
         byte[] res = person.toByteArray();
 
         System.out.println(res.length);
-        System.out.print(Hex.encodeHex(res));
+        System.out.println(Hex.encodeHex(res));
+
+        Person.Builder builder = person.toBuilder();
+        String jsonString = toJson(builder);
+        System.out.println(jsonString);
     }
 }
